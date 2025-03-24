95b27f6f2298850e91a89d7028b3361998e53644
ilYa <EnableWebSocket@yandex.ru>
2025-03-25 00:25:47 +0500

    task1

---
+++ task1/src/task1.java
@@ -0,0 +1,37 @@
+package org.pii;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int n;
+        int m;
+
+        if (args.length != 2) {
+            throw new IllegalArgumentException("2 int args: 'n' - cycle array size; 'm' - path length");
+        }
+
+        try {
+            n = Integer.parseInt(args[0]);
+            m = Integer.parseInt(args[1]);
+        } catch (NumberFormatException exc) {
+            throw new IllegalArgumentException("2 int args. Example: 4 3", exc);
+        }
+
+        System.out.println(findPath(n, m));
+    }
+
+    public static String findPath(int n, int m) {
+
+        var result = new StringBuilder();
+
+        int ptr = 0;
+
+        do {
+            result.append(ptr + 1);
+            ptr = (ptr + m - 1) % n;
+        } while (ptr != 0);
+
+        return result.toString();
+    }
+}
